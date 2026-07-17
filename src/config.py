4af598ad98fc0412f2bf5cfbd045f6a5d4d3c3e1
wang-jinchao <petercoming_863@163.com>
2026-07-17 10:48:12 +0800

    3

--- src/config.py
+++ src/config.py
@@ -1,8 +1,25 @@
 import os
 import json
+
+TARGET_URL = "https://www.upfitapp.com/year2024/rsbindex0823"
+ACCOUNTS_JSON = [
+  {
+    "name":"user001",
+    "username":"RS2789",
+    "password":"111111"
+  },
+  {
+    "name":"user002",
+    "username":"RS2790",
+    "password":"111111"
+  }
+]
+
 def load_accounts():
     return json.loads(
         os.environ["ACCOUNTS_JSON"]
     )
+
 def get_url():
     return os.environ["TARGET_URL"]
+
